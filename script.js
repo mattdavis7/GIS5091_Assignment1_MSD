6a4851052767cfc5b83211906e6674a1f8481282
mattdavis7 <112408830+mattdavis7@users.noreply.github.com>
2023-01-22 19:32:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,21 +10,16 @@ var mymap = L.map('map').setView([38.63629, -90.23640], 10);
 }).addTo(mymap);
 
 var marker1 = L.marker([38.635, -90.237]).addTo(mymap);
-
 marker1.bindPopup("<b>Resturant 1</b><br>Humphreys").openPopup();
 
 var marker2 = L.marker([38.627, -90.255]).addTo(mymap);
-
 marker2.bindPopup("<b>Resturant 2</b><br>Gramophone").openPopup();
 
 var marker3 = L.marker([38.668, -90.587]).addTo(mymap);
-
 marker3.bindPopup("<b>Store 1</b><br>Tesla").openPopup();
 
 var marker4 = L.marker([38.629, -90.292]).addTo(mymap);
-
 marker4.bindPopup("<b>Brewery 1</b><br>Heavy Riff").openPopup();
 
 var marker5 = L.marker([38.554, -90.319]).addTo(mymap);
-
 marker5.bindPopup("<b>Restaurant 3</b><br>9 Mile Garden").openPopup();
